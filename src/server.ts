67d19ee4b1d2751ddeb0fb2c527e62a50439b731
Juan Cruz Alderete <juancruzalderete11@gmail.com>
2025-11-25 21:13:01 -0300

    Implement CORS middleware with allowed origins for enhanced security and flexibility in server configuration.

--- src/server.ts
+++ src/server.ts
@@ -7,10 +7,28 @@ import { errorHandler } from "./middleware/error.middleware";
 import conversationRoutes from "./routes/conversation.routes";
 import messagesRoutes from "./routes/message.routes";
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://yap-chat-front-bojj7quyt-juanalderetes-projects.vercel.app",
+  process.env.FRONTEND_URL,
+];
+
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        return callback(new Error("Not allowed by CORS"), false);
+      }
+      return callback(null, true);
+    },
+    credentials: true,
+  })
+);
 
 app.get("/api/health", (req, res) => {
   res.status(200).json({ message: "OK" });
